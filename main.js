621774caac1d645655cc917d38e29c8995a3580d
KEVIN ALVAREZ <keymar1891@gmail.com>
2021-05-30 21:38:09 -0500

    se crean función para el objeto modelo

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
 //Funcion anonima del primer objeto modelo del Board
+(function(){
+    self.Board = function(width, height){
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+        this.playing = false;
+    }
+
+    //Se crea para obtener los prototipos Getter de la clase Board
+    self.Board.prototype = {
+        get elements(){
+            var elements = this.bars.map(function(bar){return bar;});
+            elements.push(this.ball);
+            return elements;
+        }
+    }
+})();
+
+//Funcion de la pelota, constructor
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
+
+        board.ball = this;
+        this.kind = "circle";        
+    }
+
+    //Funcion para mover la pelota
+    self.Ball.prototype = {
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        },
+
+        get width(){
+            return this.radius * 2;
+        },
+
+        get height(){
+            return this.radius * 2;
+        },
+
+      
+    }
+})();
+
+
+
+  
+
+
