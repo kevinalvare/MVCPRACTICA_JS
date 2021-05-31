3e4cb38726ee92fa6718cfc3bc28c3f1c0f8d204
KEVIN ALVAREZ <keymar1891@gmail.com>
2021-05-30 21:42:45 -0500

    se crean función para la barras y pelotas

--- main.js
+++ main.js
@@ -53,12 +53,53 @@
             return this.radius * 2;
         },
 
-      
+        collition: function(bar){
+            //Reacciona a la colision con una barra que recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if(this.x > (this.board.width / 2))
+            {
+                this.direction = -1;
+            } 
+            else
+            {
+                this.direction = 1
+            } 
+        }
     }
 })();
 
+//Constructor de la clase Bar
+(function(){
+    self.Bar = function(x,y,width, height, board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 10;
+    }
 
-
-  
-
+    //Para mover las barras
+    self.Bar.prototype = {
+        down: function(){
+            this.y += this.speed;
+        },
+        up: function(){
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: "+this.x+" y: "+this.y;
+        }
+    }
+})();
 
